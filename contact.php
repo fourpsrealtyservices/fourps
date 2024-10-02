3787425936cfa8062ab135645cd8beb10b592408
fourpsrealtyservices <fourpsrealtyservices@gmail.com>
2024-10-02 12:35:04 +0530

    init

---
+++ contact.php
@@ -0,0 +1,27 @@
+
+<?php
+// Get data from form 
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+ 
+$to = "contact@fourps.in";
+$subject = "This is the subject line";
+ 
+// The following text will be sent
+// Name = user entered name
+// Email = user entered email
+// Message = user entered message
+$txt ="Name = ". $name . "\r\n  Email = "
+    . $email . "\r\n Message =" . $message;
+ 
+$headers = "From: contact@fourps.in" . "\r\n" .
+            "CC: contact@fourps.in";
+if($email != NULL) {
+    mail($to, $subject, $txt, $headers);
+}
+ 
+// Redirect to
+header("Location:last.html");
+
+?>
